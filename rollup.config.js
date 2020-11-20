10eed3e3694d8714d30c5a94b015a34c132c1914
Elijah Mooring <ejsisgreat@gmail.com>
2020-11-20 12:50:30 -0600

    feat: bundle the source code for npm

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import builtinModules from 'builtin-modules';
+
+export default [
+  {
+    input: 'src/client/index.js',
+    plugins: [nodeResolve({ preferBuiltins: false, browser: true }), commonjs()],
+    output: [
+      {
+        format: 'esm',
+        file: 'dist/client.mjs'
+      },
+      {
+        format: 'cjs',
+        file: 'dist/client.js'
+      }
+    ],
+    onwarn: (warning, defaultHandler) => {
+      if (warning.code === 'THIS_IS_UNDEFINED') return;
+      defaultHandler(warning);
+    }
+  },
+  {
+    input: 'src/server/index.js',
+    external: builtinModules,
+    plugins: [nodeResolve({ preferBuiltins: true }), commonjs()],
+    output: [
+      {
+        format: 'esm',
+        file: 'dist/server.mjs'
+      },
+      {
+        format: 'cjs',
+        file: 'dist/server.js'
+      }
+    ]
+  }
+];
