5049744c3d6974d506f5d08051a34d3d89cb81cf
Abdullah <nyassabu@gmail.com>
2020-11-29 18:44:26 +0000

    fixed issue Error: no PRNG... option1

--- src/client/keypair/newKeypair.js
+++ src/client/keypair/newKeypair.js
@@ -5,7 +5,19 @@ import { bufferToHex } from '../../utils/index.js';
 
 nacl.util = naclutil;
 
-function newKeypair() {
+function newKeypair(_crypto_) {
+  //if _crypto_ specified it will overite the default nacl.setPRNG
+  //this only for server
+  if(_crypto_&&typeof Window==='undefined')
+  nacl.setPRNG(function(x, n){
+      let values = _crypto_.randomBytes(n)
+      if(values){
+        for (let i = 0; i < n; i++) x[i] = values[i];
+        //cleanup
+        for (let i = 0; i < values.length; i++) values[i] = 0;
+      }
+  });
+  
   const keys = nacl.box.keyPair();
   const publicKeyHex = bufferToHex(keys.publicKey);
   const privateKeyHex = bufferToHex(keys.secretKey);
