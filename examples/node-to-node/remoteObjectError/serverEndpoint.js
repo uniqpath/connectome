3dedaf36fc1daa60e2d684a3038f44b633c039ef
David Krmpotic <david.krmpotic@gmail.com>
2020-12-11 18:11:06 +0100

    delete broken examples for now

--- examples/node-to-node/remoteObjectError/serverEndpoint.js
+++
@@ -1,9 +0,0 @@
-function serverEndpoint({ channel }) {
-  channel.attachObject('ErrorObject', {
-    makeError: () => {
-      throw new Error('And just like that, AN ERROR IS THROWN');
-    }
-  });
-}
-
-export default serverEndpoint;
