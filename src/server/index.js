246a72123df238b123cfcd3d33ccee4497ec8eae
Anyass3 <nyassabu@gmail.com>
2021-02-15 00:40:23 +0000

    fixed client import issues

--- src/server/index.js
+++ src/server/index.js
@@ -2,3 +2,4 @@ import ConnectionsAcceptor from './connectionsAcceptor';
 import { newKeypair } from '../utils/crypto/index.js';
 
 export { ConnectionsAcceptor, newKeypair as newServerKeypair };
+// i guess just newKeypair was ok since there is only one.
