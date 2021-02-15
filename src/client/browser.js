d41183f8aac0b20e6b5fabd7b46e4d44ac380bef
Anyass3 <nyassabu@gmail.com>
2021-02-15 00:40:23 +0000

    fixed client import issues

---
+++ src/client/browser.js
@@ -0,0 +1,8 @@
+export { default as connectBrowser } from './connect/connectBrowser.js';
+// maybe rename connectBrowser as connect since there will be only one 'connect' in each platform.
+
+export { default as ConnectorPool } from './connectorPool/connectorPool.js';
+
+export * as concurrency from './concurrency/index.js';
+
+export { newKeypair as newClientKeypair } from '../utils/crypto/index.js';
