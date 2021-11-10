97c2bf47b3a2cc947c434405871c55ed9879ac51
David Krmpotic <david.krmpotic@gmail.com>
2021-11-10 21:36:59 +0100

    new_connectome

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
-import Connectome from './connectionsAcceptor';
+import Connectome from './connectome';
+
 import { newKeypair } from '../utils/crypto/index.js';
 
 export { Connectome, newKeypair as newServerKeypair };
