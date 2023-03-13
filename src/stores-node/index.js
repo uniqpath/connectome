54c8b8f819da17449277fb5c5fd3d66e0fa8116b
David Krmpotic <david.krmpotic@gmail.com>
2023-03-13 12:01:33 +0100

    export MCS in node as well (svelte-kit prerender fails otherwise)

--- src/stores-node/index.js
+++ src/stores-node/index.js
@@ -1,9 +1,9 @@
-// used to generate ./lib/writeFileAtomic -> index.mjs through npm run build
-// import writeFileAtomic from 'write-file-atomic';
-// export default writeFileAtomic;
-// afterwards these two lines need to be added:
-// import { fileURLToPath } from 'url';
-// const __filename = fileURLToPath(import.meta.url); // this is used in the code and just transpiling is not enough, we need to add _filename global
-
 import SyncStore from './syncStore.js';
-export { SyncStore };
+
+// we export MultiConnectedStore here because of svelte(kit) apps (and possibly other unidentified reasons)
+// when doing prerender then build tools (rollup) use node to generate code and if we don't export MCS here
+// then it will fail if our FRONTEND code uses for example: import { MultiConnectedStore } from 'connectome/stores';
+// so this only helps although MCS is not usually used from node
+import MultiConnectedStore from '../stores/lib/multiConnectedStore/multiConnectedStore.js';
+
+export { SyncStore, MultiConnectedStore };
